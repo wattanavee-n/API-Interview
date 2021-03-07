50cc0fd22faa27da9947867efdb02b527cdbf425
wattanavee-n <wattanavee.n@ku.th>
2021-03-07 23:06:25 +0700

    Add project files.

---
+++ API-Interview/Controllers/DemoEmployeeDataController.cs
@@ -0,0 +1,37 @@
+﻿using API_Interview.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Interview.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoEmployeeDataController : ControllerBase
+    {
+        private readonly ILogger<DemoEmployeeDataController> _logger;
+
+        public DemoEmployeeDataController(ILogger<DemoEmployeeDataController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public MasterDataModel Get()
+        {
+            var rng = new Random();
+            var result =  new MasterDataModel
+            {
+                Date = DateTime.Now,
+                Name = "Wattanavee Nongjan",
+                Desc = "SolutionLink Employee Senior Developer",
+                Remark = "Demo API with Swagger UI .NetCore"
+            };
+            return result;
+        }
+    }
+}
---
+++ API-Interview/Models/MasterData.model.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Interview.Models
+{
+    public class MasterDataModel
+    {
+        public DateTime Date { get; set; }
+
+        public string Name { get; set; }
+
+        public string Desc { get; set; }
+
+        public string Remark { get; set; }
+    }
+}
